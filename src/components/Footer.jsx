2e8c9d6d0a721097724d28ebf162c647e0598482
Rose <ebrue7949@gmail.com>
2023-02-16 01:19:57 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className={FooterStyle.footer}>
       <p>Copyright by Clarusway {new Date().getFullYear()}</p>
     </footer>
-  )
+  );
 }
 
 export default Footer
